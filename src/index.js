a84bd23eb5be988d8d1f3ec1a4f796ff24ecae4a
medbelka <margopv@gmail.com>
2022-11-12 16:21:49 +0200

    created Profile components

--- src/index.js
+++ src/index.js
@@ -7,4 +7,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
+// ReactDOM.render(<App />, document.querySelector('#root'));
+
