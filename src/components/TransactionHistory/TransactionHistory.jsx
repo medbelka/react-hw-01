b7e3c93d95cc37c7e2141bd93d02d11c5c47d04f
medbelka <margopv@gmail.com>
2022-11-13 14:31:24 +0200

    added TransactionHistory

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types'
+import css from 'components/TransactionHistory/TransactionHistory.module.css';
+
+export const TransactionHistory =({ items }) => {
+    return (
+        <table className={css.transactionHistory}>
+            <thead>
+                <tr>
+                <th>Type</th>
+                <th>Amount</th>
+                <th>Currency</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {items.map(({ id, type, amount, currency}) =>
+                ( <tr key={id}>
+                <td>{type}</td>
+                <td>{amount}</td>
+                <td>{currency}</td>
+                </tr> ))}
+            </tbody>
+        </table>
+    );
+};
+
+TransactionHistory.propTypes = {
+    id: PropTypes.number.isRequired, 
+    type: PropTypes.string.isRequired, 
+    amount: PropTypes.number.isRequired, 
+    currency: PropTypes.string.isRequired,
+}
